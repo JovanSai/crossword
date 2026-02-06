b94302ebbbc9c5e6c45c8c8603b0a3ac383ddb82
JovanSai <jovansai847@gmail.com>
2026-02-06 16:07:41 +0530

    Update urls.py

--- backend/hackathon/urls.py
+++ backend/hackathon/urls.py
@@ -4,13 +4,13 @@ from .views import ApiForgotPasswordView, ApiLoginView, ApiLogoutView, ApiMeView
 from . import views
 
 urlpatterns = [
-    path('', HealthView.as_view(), name='health'),
+    path('hi', HealthView.as_view(), name='health'),
     path('api/login', ApiLoginView.as_view(), name='api_login'),
     path('api/register', ApiRegisterView.as_view(), name='api_register'),
     path('api/forgot-password', ApiForgotPasswordView.as_view(), name='api_forgot_password'),
     path('api/otp/request', ApiOtpRequestView.as_view(), name='api_otp_request'),
     path('api/otp/verify', ApiOtpVerifyView.as_view(), name='api_otp_verify'),
-    path('api/home', ApiMeView.as_view(), name='api_home'),
+    path('', ApiMeView.as_view(), name='api_home'),
     path('api/logout', ApiLogoutView.as_view(), name='api_logout'),
     path('api/crossword/puzzle/<str:puzzle_id>', views.get_puzzle, name='get_puzzle'),
     path('api/crossword/submit', views.submit_puzzle, name='submit_puzzle'),
